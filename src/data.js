6f11e892baea8dc1683ed7afcc3b2ad7c0aad282
unknown <rishiwaghmode07@gmail.com>
2023-10-05 22:41:13 +0530

    This is first commit

---
+++ src/data.js
@@ -0,0 +1,6 @@
+let moviesList = ['Drishyam 2', 'Subhedar', 'vikram Vedha', 'Bawaal', 'Life of pie'];
+let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"];
+let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+exports.moviesList = moviesList;
+exports.slots = slots;
+exports.seats = seats;
